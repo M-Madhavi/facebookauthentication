54fec1618e4380634944eabffdd6e33691941d9f
M-Madhavi <62045841+M-Madhavi@users.noreply.github.com>
2020-07-24 14:17:03 +0530

    Update index.js

--- server/configuration/index.js
+++ server/configuration/index.js
@@ -3,7 +3,7 @@ module.exports = {
   oauth: {
 
     facebook: {
-      clientID: '949019605570870',
-      clientSecret: 'fef448b1a2fd8e26cc3cb5f323178f47'
+      clientID: '',
+      clientSecret: ''
     }
 }};
